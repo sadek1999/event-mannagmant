11cc55620af7837e7b17591b6aa1a00bd06a2054
Sadek Ali <sadekali1530@gmail.com>
2023-10-07 14:52:07 +0600

    successfully add navbar and log and singup page

--- src/Pages/Home/Singup.jsx
+++ src/Pages/Home/Singup.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Singup = () => {
+
     const handlSubmit=e=>{
         e.preventDefault();
        
         const form=new FormData(e.currentTarget)
         const email=form.get("email");
         const password=form.get('password')
+        const name=form.get('name')
         e.currentTarget.reset()
-        console.log(email,password)
+        console.log(email,password,name)
     }
-
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col ">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Sing up now!</h1>
                    
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form className="card-body" onSubmit={handlSubmit}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -37,14 +44,14 @@ const Login = () => {
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Sing up</button>
                         </div>
                     </form>
-                    <p>Have an account <span className='text-blue-600 font-bold'><Link to={'/singup'}>Sing Up</Link></span></p>
+                    <p>Have an account <span className='text-blue-600 font-bold'><Link to={'/login'}>Log in</Link></span></p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Singup;
--- src/Routs/Rouths.jsx
+++ src/Routs/Rouths.jsx
@@ -2,6 +2,9 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "./Main";
 import Home from '../Pages/Home/Home.jsx'
+import Login from "../Pages/Home/Login";
+import Singin from "../Pages/Home/Singup";
+import Singup from "../Pages/Home/Singup";
 
 const rouths =  createBrowserRouter([
     {
@@ -11,7 +14,16 @@ const rouths =  createBrowserRouter([
         {
           path:'/',
           element:<Home></Home>
-        }
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        },
+        {
+          path:'/singup',
+          element:<Singup></Singup>
+        },
+        
       ]
     },
   ]);
